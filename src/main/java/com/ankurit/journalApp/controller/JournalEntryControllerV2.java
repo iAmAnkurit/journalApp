2fe90f44ea1148e52938bba806048fe6683b3194
iAmAnkurit <bankurit1891@gmail.com>
2025-10-04 20:33:41 +0530

    Updated controller with http status codes.

--- src/main/java/com/ankurit/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/ankurit/journalApp/controller/JournalEntryControllerV2.java
@@ -5,13 +5,13 @@ import com.ankurit.journalApp.entity.JournalEntry;
 import com.ankurit.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
@@ -21,34 +21,50 @@ public class JournalEntryControllerV2 {
     private JournalEntryService journalEntryService;
 
     @GetMapping
-    public List<JournalEntry> getAll()
+    public ResponseEntity<List<JournalEntry>> getAll()
     {
-        return journalEntryService.getAll();
+        List<JournalEntry> journalEntryList = journalEntryService.getAll();
+        if(journalEntryList!=null && !journalEntryList.isEmpty())
+        {
+            return new ResponseEntity<List<JournalEntry>>(journalEntryList,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry)
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry)
     {
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry( myEntry );
-        return myEntry;
+        try
+        {
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry( myEntry );
+            return new ResponseEntity<JournalEntry>(myEntry,HttpStatus.CREATED) ;
+        }
+        catch(Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST) ;
+        }
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getEntryById(@PathVariable ObjectId myId)
+    public ResponseEntity<JournalEntry> getEntryById(@PathVariable ObjectId myId)
     {
-        return journalEntryService.getJournalEntryById(myId).orElse(null);
+        Optional<JournalEntry> journalEntry=journalEntryService.getJournalEntryById(myId);
+        if(journalEntry.isPresent())
+        {
+            return new ResponseEntity<JournalEntry>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<JournalEntry>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
-    public boolean deleteJournal(@PathVariable ObjectId myId)
+    public ResponseEntity<?> deleteJournal(@PathVariable ObjectId myId)
     {
         journalEntryService.deleteJournalEntry(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("id/{id}")
-    public JournalEntry updateJournalById(@PathVariable ObjectId id,@RequestBody JournalEntry myEntry)
+    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id,@RequestBody JournalEntry myEntry)
     {
         JournalEntry oldEntry = journalEntryService.getJournalEntryById(id).orElse(null);
 
@@ -56,9 +72,9 @@ public class JournalEntryControllerV2 {
         {
             oldEntry.setTitle(myEntry.getTitle()!=null && !myEntry.getTitle().equals("")?myEntry.getTitle():oldEntry.getTitle());
             oldEntry.setContent(myEntry.getContent()!=null && !myEntry.getContent().equals("")?myEntry.getContent():oldEntry.getContent());
-
+            journalEntryService.saveEntry( oldEntry );
+            return new ResponseEntity<JournalEntry>(oldEntry,HttpStatus.OK);
         }
-        journalEntryService.saveEntry( oldEntry );
-        return oldEntry;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
